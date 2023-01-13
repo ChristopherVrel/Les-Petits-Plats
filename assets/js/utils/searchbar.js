0b9500dbd788afa1bc74f5ff4eee91fa901e82d5
ChrisLamanu <christopher.vrel@gmail.com>
2023-01-13 11:34:44 +0100

    functional

--- assets/js/utils/searchbar.js
+++ assets/js/utils/searchbar.js
@@ -6,29 +6,16 @@ function searchBar(dropModel) {
     function search(e) {
         const recipesFiltered = dropModel.getFilteredRecipes();
         const searchChar = e.currentTarget.value;
-        const result = [];
 
         if (searchChar.length >= 3) {
-            for (let i = 0; i < recipesFiltered.length; i++) {
-                if (recipesFiltered[i].name.toLowerCase().indexOf(searchChar.toLowerCase()) !== -1) {
-                    result.push(recipesFiltered[i]);
-                }
-                else {
-                    if (recipesFiltered[i].description.toLowerCase().indexOf(searchChar.toLowerCase()) !== -1) {
-                        result.push(recipesFiltered[i]);
-                    }
-                    else {
-                        for (let e = 0; e < recipesFiltered[i].ingredients.length; e++) {
-                            if (recipesFiltered[i].ingredients[e].ingredient.toLowerCase().indexOf(searchChar.toLowerCase()) !== -1) {
-                                result.push(recipesFiltered[i]);
-                            }
-                        }
-                    }
-                }
-            }
+            const result = recipesFiltered.filter(e => 
+                e.name.toLowerCase().indexOf(searchChar.toLowerCase()) !== -1 ||
+                e.description.toLowerCase().indexOf(searchChar.toLowerCase()) !== -1 ||
+                e.ingredients.filter(i => i.ingredient.toLowerCase().indexOf(searchChar.toLowerCase()) !== -1).length > 0
+            );
 
             hideAndShowSortedCards(result);
-
+            
             console.log(result);
         }
         else {
